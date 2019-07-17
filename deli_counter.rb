1ddeeeb8d252d6dd0be2fbdc0cfd09c956de22d1
Tate Shepard <tateshepard@yahoo.com>
2019-07-17 03:47:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
+katz_deli = [];
+
+def line
+  
+  puts "The line is currently empty."
+end
+
+def take_a_number (katz_deli, name)
+  katz_deli.push(name)
+  number = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{number} in line."
+  
+end
+  
+
 # Write your code here.
